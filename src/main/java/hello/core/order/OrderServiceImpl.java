7be42443f3d3251fd564cd1ba3e0e6d7f04b54a9
Mygjae <vwfy@live.com>
2023-08-09 23:50:11 +0900

    의존 관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,21 +6,46 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("service")
+@Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DisCountPolicy disCountPolicy;
-    //private final DisCountPolicy disCountPolicy = new RateDiscountPolicy();
-    @Autowired
+
+/*
     public OrderServiceImpl(MemberRepository memberRepository, DisCountPolicy disCountPolicy) {
         this.memberRepository = memberRepository;
         this.disCountPolicy = disCountPolicy;
     }
+*/
+    /*
+        @Autowired
+        public void init(MemberRepository memberRepository, DisCountPolicy disCountPolicy) {
+            this.memberRepository = memberRepository;
+            this.disCountPolicy = disCountPolicy;
+        }
 
+        @Autowired(required = false)
+        public void setMemberRepository(MemberRepository memberRepository) {
+            System.out.println("memberRepository = " + memberRepository);
+            this.memberRepository = memberRepository;
+        }
+        @Autowired
+        public void setDiscountPolicy(DisCountPolicy disCountPolicy) {
+            System.out.println("disCountPolicy = " + disCountPolicy);
+            this.disCountPolicy = disCountPolicy;
+        }
+        @Autowired
+        public OrderServiceImpl(MemberRepository memberRepository, DisCountPolicy disCountPolicy) {
+            this.memberRepository = memberRepository;
+            this.disCountPolicy = disCountPolicy;
+        }
+    */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
